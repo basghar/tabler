2e8dfd03609e1983722c3a09a77158b4ad030093
Babar A Asghar <babarali@gmail.com>
2016-04-09 20:12:48 +0500

    Fixed formatting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,26 +11,27 @@ angular.module('tabler')
   .controller('MainCtrl', function($scope) {
     var minNumber = 2;
 
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     $scope.maxNumber = 13;
     $scope.state = 'init';
 
-    $scope.start = function () {
+    $scope.start = function() {
       $scope.x = getRandomInt(minNumber, $scope.maxNumber);
       $scope.y = getRandomInt(minNumber, $scope.maxNumber);
       $scope.state = 'awaitingAnswer';
     };
 
-    $scope.checkAnswer = function (x, y, answer) {
+    $scope.checkAnswer = function(x, y, answer) {
       $scope.isAnswerCorrect = answer === x * y;
       $scope.answer = null;
-      $scope.state = 'checkedAnswer.' + ($scope.isAnswerCorrect ? 'correct' : 'incorrect');
+      $scope.state = 'checkedAnswer.' +
+        ($scope.isAnswerCorrect ? 'correct' : 'incorrect');
     };
 
-    $scope.hasAnswered = function () {
+    $scope.hasAnswered = function() {
       return $scope.state.indexOf('checkedAnswer') === 0;
     };
-
-    function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
   });
